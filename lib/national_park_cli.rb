141cff165c8224214cf141dad2b9823ca3b3cf37
Brian Salemi <briandsalemi@gmail.com>
2019-06-04 19:42:27 +0000

    first commit

---
+++ lib/national_park_cli.rb
@@ -0,0 +1,6 @@
+require "national_park_cli/version"
+
+module NationalParkCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/national_park_cli/version.rb
@@ -0,0 +1,3 @@
+module NationalParkCli
+  VERSION = "0.1.0"
+end
