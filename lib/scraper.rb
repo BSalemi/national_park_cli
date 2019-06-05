7336a33d9761af10459acb98e86eeb50b2b10916
Brian Salemi <briandsalemi@gmail.com>
2019-06-05 23:01:01 +0000

    changed filename for environment, also started scraping

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,14 @@
+require 'pry'
+
 class Scraper 
   
+  NPS_INDEX = "https://www.nps.gov/index.htm"
   
+  def self.scrape_nps
+    doc = open(NPS_INDEX)
+    parsed_doc = Nokogiri::HTML(doc)
+    binding.pry
+  
+  end   
   
 end 
