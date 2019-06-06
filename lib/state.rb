08700f26a75ef6c48568dc9a36b1c1037fa09d39
Brian Salemi <briandsalemi@gmail.com>
2019-06-06 16:18:42 +0000

    State class set up

--- lib/state.rb
+++ lib/state.rb
@@ -1,5 +1,21 @@
 class State 
   
+  attr_accessor :name, :abbreviation, :url, :parks_scraped
+  
+  @@all = [] 
+  
+    def initialize(name, abbreviation, url, parks_scraped = false)
+      @name = name
+      @abbreviation = abbreviation 
+      @url = url 
+      @parks_scraped = parks_scraped
+      @@all << self 
+    end 
+    
+    def self.all 
+      @@all 
+    end 
+  
   
   
 end 
